27626adc74b910a27633de73b7aa6e3599df865b
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-22 20:41:37 +0530

    initialized the Indian census analyser problem

---
+++ lib/src/main/java/IndianStateCensusAnalyserProblem/CensusAnalyser.java
@@ -0,0 +1,19 @@
+package IndianStateCensusAnalyserProblem;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CensusAnalyser {
+
+	public static void main (String args[])
+	{
+		System.out.println("Welcome to Indian censun analyser");
+	}
+}
