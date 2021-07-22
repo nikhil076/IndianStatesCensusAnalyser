2c469ae412baccd239e1d2bdb8714f42ececbc88
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-22 20:44:46 +0530

    addded the functionality to read from the csv file to bean

--- lib/src/main/java/IndianStateCensusAnalyserProblem/CensusAnalyser.java
+++ lib/src/main/java/IndianStateCensusAnalyserProblem/CensusAnalyser.java
@@ -16,4 +16,20 @@ public class CensusAnalyser {
 	{
 		System.out.println("Welcome to Indian censun analyser");
 	}
+	public int loadIndianStateCensusData(String filePath) throws IOException
+	{
+			Reader reader =Files.newBufferedReader(Paths.get(filePath));
+			CsvToBeanBuilder<IndianCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndianCensusCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndianCensusCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndianCensusCSV> censusCSVIterator = csvToBean.iterator();
+			int numberOfEntries =0;
+			while(censusCSVIterator.hasNext())
+			{
+				numberOfEntries++;
+				IndianCensusCSV censusCSV = censusCSVIterator.next();
+			}
+		return numberOfEntries;
+	}
 }
